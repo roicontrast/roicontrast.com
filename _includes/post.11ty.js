6f76addb23587b0c12ded108c04ad826a430434e
Ryan Chute <rchute@shopzilla.com>
2020-10-28 22:33:55 -0700

    Cleanup

--- _includes/post.11ty.js
+++ _includes/post.11ty.js
@@ -19,7 +19,7 @@ module.exports = (data) => {
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>${title} - My Blog</title>
+        <title>${title} - Learnings</title>
         <meta name="description" content="${headline}"/>
         <link rel="stylesheet" href="/css/global.css" />
     </head>
